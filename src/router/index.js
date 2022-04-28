64019c89f18cbf3544628f454dc21adac711bc46
lura_syxilia <1329317082a@gmail.com>
2022-04-28 10:19:23 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,81 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// 一级路由
+import Home from '../views/Home/Home.vue'
+import Mine from '../views/Mine'
+import Address from '../views/Address'
+import Car from '../views/ShoppingCar'
+import Category from '../views/Category'
+import Account from '../views/Account'
+// 二级路由
+import Login from '../views/Mine/Login'
+import LogByPhone from '../views/Mine/Login/Byphone.vue'
+import Register from '../views/Mine/Register'
+import User from '../views/Mine/User'
+import MyAddress from '../views/Address/MyAddress'
+import ModAddress from '../views/Address/ModAddress'
+import MyAccount from '../views/Account/MyAccount'
+import UserMod from '../views/Account/UserMod'
+import Order from '../views/Account/Order'
 
 const routes = [
+  { path: '/', redirect: '/home' },
   {
-    path: '/',
+    path: '/home',
     name: 'home',
-    component: HomeView
+    component: Home,
+    meta: { showH: true, showF: true },
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/mine',
+    name: 'mine',
+    component: Mine,
+    meta: { showH: false, showF: true },
+    children: [
+      { path: '', component: User },
+      { path: 'logphone', component: LogByPhone },
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+    ],
+  },
+  {
+    path: '/car',
+    name: 'car',
+    component: Car,
+    meta: { showH: false, showF: true },
+  },
+  {
+    path: '/address',
+    name: 'address',
+    component: Address,
+    meta: { showH: false, showF: false },
+    children: [
+      { path: '', component: MyAddress },
+      { name: 'mod', path: 'mod', component: ModAddress },
+    ],
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: Category,
+    meta: { showH: true, showF: true },
+  },
+  {
+    path: '/account',
+    name: 'account',
+    component: Account,
+    meta: { showH: false, showF: false },
+    children: [
+      { path: '', component: MyAccount },
+      { path: 'mod', component: UserMod },
+      { path: 'order', component: Order },
+    ],
+  },
 ]
+// 待完成：前置导航守卫（拦截未登录进入权限界面）
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 })
 
 export default router
