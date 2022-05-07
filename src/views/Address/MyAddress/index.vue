430f0a8f52e05ea5b98e1a729ee3f95911418a4c
lura_syxilia <1329317082a@gmail.com>
2022-05-07 17:01:05 +0800

    修复地址编辑bug

--- src/views/Address/MyAddress/index.vue
+++ src/views/Address/MyAddress/index.vue
@@ -1,6 +1,11 @@
 <template>
+  <van-cell
+    style="margin-top: 10px"
+    v-show="!$store.state.User.addressList.address.length"
+    title="您还没收获地址，赶紧来设置一个吧"
+  />
   <van-address-list
-    v-model="currentId"
+    v-model="$store.state.User.addressList.current.id"
     :list="$store.state.User.addressList.address"
     default-tag-text="默认"
     @add="modAdd"
@@ -9,24 +14,11 @@
 </template>
 
 <script>
-import { getAddress } from '@/api'
-import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import userStore from '@/store/user'
 export default {
   name: 'MyAddress',
   setup() {
     // console.log('myaddress')
-    const currentId = ref(-1)
-    setTimeout(() => {
-      if (!userStore.state.visitedAddress) {
-        getAddress(userStore.state.user.userid)
-      }
-    }, 200)
-
-    setTimeout(() => {
-      currentId.value = userStore.state.addressList.current.id
-    }, 300)
     const router = useRouter()
     const modAdd = () => {
       router.push({ path: '/address/mod', query: { type: 'add' } })
@@ -44,7 +36,7 @@ export default {
       })
     }
     // @select="changeDef"
-    return { currentId, modAdd, editAdd }
+    return { modAdd, editAdd }
   },
 }
 </script>
